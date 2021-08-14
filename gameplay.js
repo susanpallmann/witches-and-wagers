c4f63a0406dae484fe8ddc616a77c9472b2b1634
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-08-14 18:17:46 -0400

    Update gameplay.js

--- gameplay.js
+++ gameplay.js
@@ -231,9 +231,9 @@ function sortLoadingCards(values, creator) {
     let player = values.player;
     let location;
     if (creator === 'playerItems') {
-        location = $('#cards-in-play-2');
+        location = '#cards-in-play-2';
     } else if (creator === 'audienceItems') {
-        location = $('#cards-in-play-1');
+        location = '#cards-in-play-1';
     }
     clearCardsDom(location);
     console.log('deleted at ' + location);
@@ -241,7 +241,7 @@ function sortLoadingCards(values, creator) {
         let thisActor = actor;
         let thisCardsArray = values[actor];
         for (let i = 0; i < thisCardsArray.length; i++) {
-            createCardDom($(location),{'number' : thisCardsArray[i], 
+            createCardDom($('#' + location),{'number' : thisCardsArray[i], 
                                     'assigned' : thisActor});
         }
     }

c849ddd60c33c7e90aa128240c4ff4053e4ae71d
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-08-14 23:24:56 -0400

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,53 +1,414 @@
 // Title
-// Type (1 = both, 2 = player only, 3 = monster only)
+// User (1 = both, 2 = player only, 3 = monster only)
+// Type (1 = item, 2 = weapon, 3 = armor)
 // Image
 // Description
 // Point change
 // Gold worth
 let deck = {
-  1: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  2: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  3: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  4: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  5: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  6: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  7: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  8: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  9: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  10: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  11: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  12: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  13: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  14: ['Crystal of Fortitude', 1 , 2 , "If nothing else, it makes for a shiny room decoration.", 5, 10],
-  15: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  16: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  17: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  18: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  19: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  20: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  21: ['Mysterious Potion', 1 , 3 , "Legally, we cannot recommend drinking this.", 4, 8],
-  22: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  23: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  24: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  25: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  26: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  27: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  28: ['Banana Peel', 1 , 4 , "How careless to just leave this lying around?", -3, 4],
-  29: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  30: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  31: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  32: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  33: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  34: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  35: ['Emerald Feather', 1 , 5 , "Perhaps you'll be luckier than the bird this once belonged to.", 1, 2],
-  36: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  37: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  38: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  39: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  40: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  41: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  42: ['Health Potion', 1 , 6 , "* side effects stull unstudied", 2, 1],
-  43: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  44: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4],
-  45: ['A Full Pint', 1 , 1 , "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.", 2, 4]
+  1 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  2 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  3 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  4 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  5 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  6 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  7 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  8 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  9 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  10 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  11 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  12 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  13 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  14 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  15 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  16 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  17 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  18 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  19 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  20 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  21 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  22 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  23 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  24 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  25 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  26 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  27 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  28 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  29 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  30 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  31 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  32 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  33 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  34 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  35 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  36 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  37 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  38 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  39 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  },
+  40 : {
+    title : 'Banana Peel',
+    user : 1,
+    type : 1,
+    sprite : 4,
+    description : "How careless to just leave this lying around?",
+    effect : -3,
+    gold : 4
+  },
+  41 : {
+    title : 'Emerald Feather',
+    user : 1,
+    type : 1,
+    sprite : 5,
+    description : "Perhaps you'll be luckier than the bird this once belonged to.",
+    effect : 1,
+    gold : 2
+  },
+  42 : {
+    title : 'Health Potion',
+    user : 1,
+    type : 1,
+    sprite : 6,
+    description : "* side effects still unstudied",
+    effect : 2,
+    gold : 1
+  },
+  43 : {
+    title : 'A Full Pint',
+    user : 1,
+    type : 1,
+    sprite : 1,
+    description : "Much like a potion, it grants you strong powers for a short time... and a bit of a headache.",
+    effect : 2,
+    gold : 4
+  },
+  44 : {
+    title : 'Crystal of Fortitude',
+    user : 1,
+    type : 1,
+    sprite : 2,
+    description : "If nothing else, it makes for a shiny room decoration.",
+    effect : 5,
+    gold : 10
+  },
+  45 : {
+    title : 'Mysterious Potion',
+    user : 1,
+    type : 1,
+    sprite : 3,
+    description : "Legally, we cannot recommend drinking this.",
+    effect : 4,
+    gold : 8
+  }
 };

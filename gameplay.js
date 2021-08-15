6949065027dfb186038df06a416478d6900caba6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-08-15 00:08:16 -0400

    Update gameplay.js

--- gameplay.js
+++ gameplay.js
@@ -285,7 +285,7 @@ $(document).ready(function() {
     displayAllStats('TEST');
     // TODO: Call during the scoreboard phase of the play phase
     displayScoreboard('TEST');
-    loadCardDisplay('TEST', 'audienceItems');
+    //loadCardDisplay('TEST', 'audienceItems');
     loadCardDisplay('TEST', 'playerItems');
 });
 

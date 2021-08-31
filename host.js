95e1f3f67a416aac530971e8b8a0448b155b72ad
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-08-31 00:34:02 -0400

    Update host.js

--- host.js
+++ host.js
@@ -138,7 +138,7 @@ function generateRoomCode(code, callback) {
         roomCode =  code + newLetter;
 
         // Run this function again to check if the code is complete now (length of 4)
-        generateRoomCode(roomCode, callback);
+        generateRoomCode(roomCode, verifyRoomCode);
         
     // If the string is 4 characters
     } else {

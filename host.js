87ffc939615645707b7d327647e031c9853bbfe2
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-09-01 12:34:01 -0400

    Update host.js

--- host.js
+++ host.js
@@ -721,8 +721,8 @@ $(document).ready(function() {
     // TODO: Call during the scoreboard phase of the play phase
     displayScoreboard('TEST');
     
-    //loadCardDisplay('TEST', 'audienceItems');
-    loadCardDisplay('TEST', 'playerItems');
+    loadCardDisplay('TEST', 'audienceItems');
+    //loadCardDisplay('TEST', 'playerItems');
     
     // TODO: Call during play phase when fight is introduced
     displayActorScores('TEST');

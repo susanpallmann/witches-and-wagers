5e5c7fcf156d6c1e781508789e8ef2be3bec2ee6
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-09-01 17:30:38 -0400

    Update host.js

--- host.js
+++ host.js
@@ -653,7 +653,7 @@ function createCardDom(code, location, cardInfo) {
     
     // Finds the requested card in our deck object (see deck.js)
     let cardLookup = deck[number];
-    console.log(cardLookup);
+    console.log(number);
     
     // Looks up sprite and score modifier from deck object
     let cardSprite = cardLookup.sprite;

6d976e0623005dca4c8a8840b8795bd6e45acc25
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-09-03 23:47:02 -0400

    Update host.js

--- host.js
+++ host.js
@@ -848,6 +848,6 @@ $(document).ready(function() {
     // Cycles to next player
     updateCurrentPlayer('TEST');
     
-    removePlayer('TEST', 'Auben');
+    removePlayer('TEST', 'Skooz');
 });
 // I miss you every day

53ad4abdfdfae2c3b265acb72dac8ba57b09eb26
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-09-04 20:46:43 -0400

    Update common.js

--- common.js
+++ common.js
@@ -555,7 +555,7 @@ function returnPlayItems(code, playerName) {
         let thisPlayer;
         console.log(thisPlayer);
         console.log(playersArray.length);
-        for (let i; i < playersArray.length; i++) {
+        for (let i = 0; i < playersArray.length; i++) {
             thisPlayer = playersArray[i];
             console.log(thisPlayer);
             returnBidItems(code, thisPlayer, `/players/${thisPlayer}/inventory`);

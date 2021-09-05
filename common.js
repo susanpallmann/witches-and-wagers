d670cd8a4f1865f478735c65022b36cf2a6c6b5c
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-09-04 20:38:37 -0400

    Update common.js

--- common.js
+++ common.js
@@ -544,9 +544,13 @@ function returnPlayItems(code, playerName) {
 
                     // If player was current player, we want to return the other players' items to them
                     let playersArray = Object.keys(players);
+                    console.log(playersArray);
                     let currentPlayer = playersArray.indexOf(playerName);
+                    console.log(currentPlayer);
                     playersArray.splice(currentPlayer, 1);
+                    console.log(playersArray);
                     let thisPlayer;
+                    console.log(thisPlayer);
                     for (let i; i < playersArray.length; i++) {
                         thisPlayer = playersArray[i];
                         console.log(thisPlayer);
